714eb97a81a39737937808163c42ba1f9427e2b4
Mandhakini N <nallapanenimandhakini@gmail.com>
2020-07-28 18:15:52 +0530

    execute

---
+++ eclipse-workspace/Calculator/src/main/java/epam3/Add.java
@@ -0,0 +1,15 @@
+package epam3;
+public class Add extends Cal_abstract{
+	public int cal(int a,int b,int c)
+	{
+		c=a+b;
+		return c;
+	}
+}package epam3;
+public class Add extends Cal_abstract{
+	public int cal(int a,int b,int c)
+	{
+		c=a+b;
+		return c;
+	}
+}
